c9f9dcbc7f46159098f267ef782bd2ab3a5dad28
Alisa Ivankina <90556933+AlirorKa@users.noreply.github.com>
2021-11-15 17:46:39 +0700

    Update tests.java

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.Test;
-import project.Main;
+import project.stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
