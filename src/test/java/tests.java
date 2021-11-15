65820e236bae466a3559f74936111bd61718798c
Alisa Ivankina <90556933+AlirorKa@users.noreply.github.com>
2021-11-15 18:32:55 +0700

    Update tests.java

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class tests {
 
     @Test
-    void empty_Stack_test() throws Exception{
+    void emptyStack() throws Exception{
         Main q = new Main();   
 
         Exception e = assertThrows(Exception.class, () -> {Integer a = (Integer) q.pop();});
@@ -26,7 +26,7 @@ public class tests {
     }
 
     @Test
-    void func_working_Integer_test() throws Exception {
+    void integerStack_1() throws Exception {
         stack<Integer> q = new stack();
         q.push((Integer)1);
         q.push((Integer)2);
@@ -42,7 +42,7 @@ public class tests {
         assert(x == 1);
     }
     @Test
-    void func2_working_Integer_test() throws Exception{
+    void integerStack_2() throws Exception{
         stack<Integer> q = new stack();
         stack<Integer> a = new stack();
         Integer x;
@@ -69,7 +69,7 @@ public class tests {
     }
 
     @Test
-    void func_working_String_test() throws Exception {
+    void stringStack() throws Exception {
         stack<String> q = new stack();
 
         q.push((String)"1");
