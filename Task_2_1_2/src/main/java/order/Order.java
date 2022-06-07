0f74be35224b00638f7c6842f8a70b2a4e7ff052
Alisa Ivankina <90556933+AlirorKa@users.noreply.github.com>
2022-06-07 10:35:57 +0700

    Add files via upload

---
+++ Task_2_1_2/src/main/java/order/Order.java
@@ -0,0 +1,24 @@
+package order;
+
+import lombok.Getter;
+import lombok.var;
+
+@Getter
+public class Order {
+    int id;
+    OrderStatus status;
+    int deliveryTime;
+
+    public Order(int id, int deliveryTime) {
+        this.id = id;
+        this.status = OrderStatus.WAIT_COOK;
+        this.deliveryTime = deliveryTime;
+        System.out.printf("Order %d created\n", id);
+    }
+
+    public void setStatus(OrderStatus status) {
+        var oldStatus = this.status;
+        this.status = status;
+        System.out.printf("Order %d status changed from %s to %s \n", id, oldStatus, status);
+    }
+}
